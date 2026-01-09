d911b285eac653d6f1e60284c3c0dc774a50d8ab
rfeenstra <reuelfeenstra@gmail.com>
2026-01-09 15:29:26 -0500

    Tweak show page

--- resources/views/check/show.blade.php
+++ resources/views/check/show.blade.php
@@ -5,7 +5,7 @@
         <h1 class="text-4xl font-bold">Result</h1>
         <p>{{ $check->oilChangeIsDue() ? 'An oil change is needed.' : 'No oil change needed at this time.' }}</p>
         <p>Current odometer reading: {{ $check->current_odometer }}</p>
-        <p>Previous oil change date: {{ $check->previous_date?->diffForHumans() }}</p>
+        <p>Previous oil change was: {{ $check->previous_date?->diffForHumans() }}</p>
         <p>Previous odometer reading: {{ $check->previous_odometer }}</p>
     </div>
 @endsection
