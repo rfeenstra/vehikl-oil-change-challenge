ffbb83d1eede850e203d5b2fd4056bf7c29eb334
rfeenstra <reuelfeenstra@gmail.com>
2026-01-09 14:33:07 -0500

    Add more validation tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,22 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Testing\TestResponse;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->registerValidationErrorMacro();
+    }
+
+    protected function registerValidationErrorMacro(): void
+    {
+        TestResponse::macro('assertValidationError', function ($key, $message) {
+            $this->assertJsonValidationErrorFor($key);
+            $this->assertJsonFragment([$key => [$message]]);
+        });
+    }
 }
--- tests/Unit/Http/Controllers/VehicleCheckControllerTest.php
+++ tests/Unit/Http/Controllers/VehicleCheckControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Http\Controllers;
 
+use App\Models\VehicleCheck;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -22,6 +23,8 @@ class VehicleCheckControllerTest extends TestCase
 
     public function test_that_guests_can_store_a_vehicle_oil_check(): void
     {
+        $this->assertSame(0, VehicleCheck::count());
+
         $response = $this->post('/check', [
             'current_odometer' => 10000,
             'previous_date' => '2025-01-01',
@@ -34,5 +37,141 @@ class VehicleCheckControllerTest extends TestCase
             'previous_date' => '2025-01-01 00:00:00',
             'previous_odometer' => 9000,
         ]);
+        $this->assertSame(1, VehicleCheck::count());
+    }
+
+    public function test_that_current_odometer_reading_is_required(): void
+    {
+        $this->assertSame(0, VehicleCheck::count());
+
+        $response = $this->postJson('/check', [
+            'current_odometer' => '',
+            'previous_date' => '2025-01-01',
+            'previous_odometer' => 9000,
+        ]);
+
+        $response->assertUnprocessable();
+        $response->assertValidationError('current_odometer', 'The current odometer field is required.');
+        $this->assertSame(0, VehicleCheck::count());
+    }
+
+    public function test_that_current_odometer_reading_must_be_a_valid_integer(): void
+    {
+        $this->assertSame(0, VehicleCheck::count());
+
+        $response = $this->postJson('/check', [
+            'current_odometer' => 'invalid',
+            'previous_date' => '2025-01-01',
+            'previous_odometer' => 10000,
+        ]);
+
+        $response->assertUnprocessable();
+        $response->assertValidationError('current_odometer', 'The current odometer must be an integer.');
+        $this->assertSame(0, VehicleCheck::count());
+    }
+
+    public function test_that_current_odometer_reading_must_be_greater_than_previous_odometer_reading(): void
+    {
+        $this->assertSame(0, VehicleCheck::count());
+
+        $response = $this->postJson('/check', [
+            'current_odometer' => 9000,
+            'previous_date' => '2025-01-01',
+            'previous_odometer' => 10000,
+        ]);
+
+        $response->assertUnprocessable();
+        $response->assertValidationError('current_odometer', 'The current odometer must be greater than the previous odometer reading.');
+        $this->assertSame(0, VehicleCheck::count());
+    }
+
+    public function test_that_previous_date_is_required(): void
+    {
+        $this->assertSame(0, VehicleCheck::count());
+
+        $response = $this->postJson('/check', [
+            'current_odometer' => 10000,
+            'previous_date' => '',
+            'previous_odometer' => 9000,
+        ]);
+
+        $response->assertUnprocessable();
+        $response->assertValidationError('previous_date', 'The previous date field is required.');
+        $this->assertSame(0, VehicleCheck::count());
+    }
+
+    public function test_that_previous_date_must_be_a_valid_date(): void
+    {
+        $this->assertSame(0, VehicleCheck::count());
+
+        $response = $this->postJson('/check', [
+            'current_odometer' => 10000,
+            'previous_date' => 'invalid',
+            'previous_odometer' => 9000,
+        ]);
+
+        $response->assertUnprocessable();
+        $response->assertValidationError('previous_date', 'The previous date field must be a valid date.');
+        $this->assertSame(0, VehicleCheck::count());
+    }
+
+    public function test_that_previous_date_must_be_before_today(): void
+    {
+        $this->assertSame(0, VehicleCheck::count());
+
+        $response = $this->postJson('/check', [
+            'current_odometer' => 10000,
+            'previous_date' => now()->addDay()->format('Y-m-d'),
+            'previous_odometer' => 9000,
+        ]);
+
+        $response->assertUnprocessable();
+        $response->assertValidationError('previous_date', 'The previous date field must be a date before today.');
+        $this->assertSame(0, VehicleCheck::count());
+    }
+
+    public function test_that_previous_odometer_is_required(): void
+    {
+        $this->assertSame(0, VehicleCheck::count());
+
+        $response = $this->postJson('/check', [
+            'current_odometer' => 10000,
+            'previous_date' => '2025-01-01',
+            'previous_odometer' => '',
+        ]);
+
+        $response->assertUnprocessable();
+        $response->assertValidationError('previous_odometer', 'The previous odometer field is required.');
+        $this->assertSame(0, VehicleCheck::count());
+    }
+
+    public function test_that_previous_odometer_must_be_a_valid_integer(): void
+    {
+        $this->assertSame(0, VehicleCheck::count());
+
+        $response = $this->postJson('/check', [
+            'current_odometer' => 10000,
+            'previous_date' => '2025-01-01',
+            'previous_odometer' => 'invalid',
+        ]);
+
+        $response->assertUnprocessable();
+        $response->assertValidationError('previous_odometer', 'The previous odometer must be an integer.');
+        $this->assertSame(0, VehicleCheck::count());
+    }
+
+    public function test_that_previous_odometer_must_be_greater_than_0(): void
+    {
+        $this->assertSame(0, VehicleCheck::count());
+
+        $response = $this->postJson('/check', [
+            'current_odometer' => 10000,
+            'previous_date' => '2025-01-01',
+            'previous_odometer' => -1,
+        ]);
+
+        $response->assertUnprocessable();
+        $response->assertValidationError('previous_odometer', 'The previous odometer field must be greater than or equal to 0.');
+        $this->assertSame(0, VehicleCheck::count());
     }
 }
